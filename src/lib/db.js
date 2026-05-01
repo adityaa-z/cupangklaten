4ebb0de5abb0669210c5c4eab14b01416282effc
unknown <zidanp13794@gmail.com>
2026-05-01 17:39:08 +0700

    Change fallback MySQL host to 127.0.0.1

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,7 +5,7 @@ let pool = null;
 export function getPool() {
     if (!pool) {
         pool = mysql.createPool({
-            host: process.env.MYSQL_HOST || 'localhost',
+            host: process.env.MYSQL_HOST || '127.0.0.1',
             port: parseInt(process.env.MYSQL_PORT || '3306'),
             user: process.env.MYSQL_USER || 'root',
             password: process.env.MYSQL_PASSWORD || '',
